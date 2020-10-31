52fc46a28f223c856905a1e043e76d1b8da2eb5a
MasatoshiShiozawa <victorynov21@gmail.com>
2020-10-31 17:08:01 +0000

    1101STEP１提出

--- spec/system/task.spec.rb
+++ spec/system/task.spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 describe 'タスク管理機能', type: :system do
+  describe '新規作成機能' do
+    context 'タスクを新規作成した場合' do
+      it '作成したタスクが表示される' do
+        visit new_task_path
+        fill_in 'Title', with: 'task_title'
+        fill_in 'Content', with: 'task_content'
+        click_button 'Create Task'
+        visit tasks_path
+        expect(page).to have_content 'task'
+      end
+    end
+  end
   describe '一覧表示機能' do
     context '一覧画面に遷移した場合' do
       it '作成済みのタスク一覧が表示される' do
@@ -14,4 +26,14 @@ describe 'タスク管理機能', type: :system do
       end
     end
   end
+  describe '詳細表示機能' do
+   context '任意のタスク詳細画面に遷移した場合' do
+     it '該当タスクの内容が表示される' do
+       @task = FactoryBot.create(:task,title: 'task1',content: 'content1')
+       visit task_path(@task)
+       expect(page).to have_content 'task1'
+       expect(page).to have_content 'content1'
+     end
+   end
+end
 end
