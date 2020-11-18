6e0064337a30dd46d777ff247c2777e7267f65c4
MasatoshiShiozawa <victorynov21@gmail.com>
2020-11-18 15:38:12 +0000

    1119seedデータ再

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,64 +1,64 @@
-2.times do |n|
-  name = Faker::Games::Pokemon.name
-  email = Faker::Internet.email
-  password = "suzuki"
-  User.create!(name: name,
-               email: email,
-               password: password,
-               password_confirmation: password,
-               admin: true)
-end
-
-8.times do |n|
-  name = Faker::Games::Pokemon.name
-  email = Faker::Internet.email
-  password = "suzuki"
-  User.create!(name: name,
-               email: email,
-               password: password,
-               password_confirmation: password,
-               admin: false)
-end
-
-Label.create!(
-  name: 'あ'
-)
-
-Label.create!(
-  name: 'い'
-)
-
-Label.create!(
-  name: 'う'
-)
-
-Label.create!(
-  name: 'え'
-)
-
-Label.create!(
-  name: 'お'
-)
-
-Label.create!(
-  name: 'か'
-)
-
-Label.create!(
-  name: 'き'
-)
-
-Label.create!(
-  name: 'く'
-)
-
-Label.create!(
-  name: 'け'
-)
-
-Label.create!(
-  name: 'こ'
-)
+# 2.times do |n|
+#   name = Faker::Games::Pokemon.name
+#   email = Faker::Internet.email
+#   password = "suzuki"
+#   User.create!(name: name,
+#                email: email,
+#                password: password,
+#                password_confirmation: password,
+#                admin: true)
+# end
+#
+# 8.times do |n|
+#   name = Faker::Games::Pokemon.name
+#   email = Faker::Internet.email
+#   password = "suzuki"
+#   User.create!(name: name,
+#                email: email,
+#                password: password,
+#                password_confirmation: password,
+#                admin: false)
+# end
+#
+# Label.create!(
+#   name: 'あ'
+# )
+#
+# Label.create!(
+#   name: 'い'
+# )
+#
+# Label.create!(
+#   name: 'う'
+# )
+#
+# Label.create!(
+#   name: 'え'
+# )
+#
+# Label.create!(
+#   name: 'お'
+# )
+#
+# Label.create!(
+#   name: 'か'
+# )
+#
+# Label.create!(
+#   name: 'き'
+# )
+#
+# Label.create!(
+#   name: 'く'
+# )
+#
+# Label.create!(
+#   name: 'け'
+# )
+#
+# Label.create!(
+#   name: 'こ'
+# )
 
 50.times do |n|
   title = Faker::Games::Pokemon.name
@@ -67,7 +67,7 @@ Label.create!(
   status = ["0","1","2"]
   Task.create!(title: title,
               content: content,
-              deadline: date,
+              deadline: deadline,
               status: rand(0..2),
               priority: rand(0..2),
               user_id: rand(1..20))
